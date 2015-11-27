c0a45c96ab30135091151ce88dec826f51c47e75
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2015-11-27 01:44:15 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,13 +3,14 @@
 # TODO: this should probably be a makefile
 # TODO: install target
 (
+# Compile dpdk
+cd ./dpdk
+make -j 8 install T=x86_64-native-linuxapp-gcc
+
+# Compile pktgen
 export RTE_SDK=./dpdk
 export RTE_TARGET=x86_64-native-linuxapp-gcc
 
-# Compile dpdk
-cd $RTE_SDK
-make -j 8 install T=$RTE_TARGET
-
-# Compile pktgen
+cd ../
 make -j 8
 )
